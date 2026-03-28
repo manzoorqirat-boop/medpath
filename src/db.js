047e7c129ddc31f53b50c91c24be62984abfc0af
manzoorqirat-boop <manzoorqirat@gmail.com>
2026-03-28 20:18:21 +0530

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 const logger   = require("./utils/logger");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL || "postgresql://postgres:lkyPnnqpIMbAViDTXmVUEJSclBOlGydv@gondola.proxy.rlwy.net:32399/railway",
   ssl: process.env.NODE_ENV === "production"
     ? { rejectUnauthorized: false }
     : false,

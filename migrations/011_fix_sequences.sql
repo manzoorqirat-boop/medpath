e76229f4155f6e85dab18f68597de2e12d61c55d
manzoorqirat-boop <manzoorqirat@gmail.com>
2026-03-30 06:22:45 +0530

    Update 011_fix_sequences.sql

--- migrations/011_fix_sequences.sql
+++ migrations/011_fix_sequences.sql
@@ -1,11 +1,41 @@
--- Fix staff_no sequence out of sync
-SELECT setval('seq_staff_no', (SELECT MAX(CAST(SPLIT_PART(staff_no, '-', 2) AS INTEGER)) FROM staff WHERE staff_no LIKE 'STF-%'), true);
+-- Fix all sequences safely using DO blocks
 
--- Fix other sequences too just in case
-SELECT setval('seq_patient_no', COALESCE((SELECT MAX(CAST(SPLIT_PART(patient_no, '-', 2) AS INTEGER)) FROM patients WHERE patient_no LIKE 'PAT-%'), 0) + 1, false);
+DO $$
+DECLARE max_staff INTEGER;
+BEGIN
+  SELECT COALESCE(MAX(CAST(SPLIT_PART(staff_no,'-',2) AS INTEGER)),0)
+  INTO max_staff FROM staff WHERE staff_no ~ '^STF-[0-9]+$';
+  PERFORM setval('seq_staff_no', max_staff+1, false);
+END $$;
 
-SELECT setval('seq_invoice_no', COALESCE((SELECT MAX(CAST(SPLIT_PART(invoice_no, '-', 4) AS INTEGER)) FROM invoices WHERE invoice_no LIKE 'INV-%'), 0) + 1, false);
+DO $$
+DECLARE max_val INTEGER;
+BEGIN
+  SELECT COALESCE(MAX(CAST(SPLIT_PART(patient_no,'-',2) AS INTEGER)),0)
+  INTO max_val FROM patients WHERE patient_no ~ '^PAT-[0-9]+$';
+  PERFORM setval('seq_patient_no', max_val+1, false);
+END $$;
 
-SELECT setval('seq_sample_no', COALESCE((SELECT MAX(CAST(SPLIT_PART(sample_no, '-', 4) AS INTEGER)) FROM samples WHERE sample_no LIKE 'SMP-%'), 0) + 1, false);
+DO $$
+DECLARE max_val INTEGER;
+BEGIN
+  SELECT COALESCE(MAX(CAST(SPLIT_PART(invoice_no,'-',3) AS INTEGER)),0)
+  INTO max_val FROM invoices WHERE invoice_no ~ '^INV-[0-9]+-[0-9]+$';
+  PERFORM setval('seq_invoice_no', max_val+1, false);
+END $$;
 
-SELECT setval('seq_report_no', COALESCE((SELECT MAX(CAST(SPLIT_PART(report_no, '-', 4) AS INTEGER)) FROM reports WHERE report_no LIKE 'RPT-%'), 0) + 1, false);
+DO $$
+DECLARE max_val INTEGER;
+BEGIN
+  SELECT COALESCE(MAX(CAST(SPLIT_PART(sample_no,'-',3) AS INTEGER)),0)
+  INTO max_val FROM samples WHERE sample_no ~ '^SMP-[0-9]+-[0-9]+$';
+  PERFORM setval('seq_sample_no', max_val+1, false);
+END $$;
+
+DO $$
+DECLARE max_val INTEGER;
+BEGIN
+  SELECT COALESCE(MAX(CAST(SPLIT_PART(report_no,'-',3) AS INTEGER)),0)
+  INTO max_val FROM reports WHERE report_no ~ '^RPT-[0-9]+-[0-9]+$';
+  PERFORM setval('seq_report_no', max_val+1, false);
+END $$;
